fed6d6125cf4a827f16ad0636f65cf8971ae354d
Carlos Arturo <carlox216@utp.edu.co>
2019-03-28 11:08:31 -0500

    Setup examples code, circuits and project 0 startup
    Setup Github Repo, IoT course initial Examples, Circuits with Frizting (Image and Fritzing file), Improme cde Documentation

---
+++ Circuits/README.md
@@ -0,0 +1,5 @@
+# Circuits With Firtzing and Images
+Circuits made with Fritzing, used extra libraries how
+* [DHT11 Sensor Module](https://github.com/HuangYuTse/DHT11-module-fritzing-part/blob/master/DHT11%20module.fzpz)
+* [Hygrometer FC-28](https://github.com/Teutatis/Fritzing-Parts)
+* [ADS1115](https://github.com/adafruit/Fritzing-Library/blob/master/parts/Adafruit%20ADS1115%2016Bit%20I2C%20ADC.fzpz)
---
+++ Example code/GPIO Pins/gpio example.py
@@ -0,0 +1,37 @@
+# Import standard Python Modules
+import time
+
+# Import RPi.GPIO Module
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+	
+# Define Function "main", way to manage errors
+def main():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	
+	# List with all GPIO pin numbers
+	pinList=[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+	
+	# Set GPIO pin signal OUT and initial value "shutdown"
+	GPIO.setup(pinList, GPIO.OUT, initial=GPIO.LOW)
+	
+	# Set signal up to all pins one to one in order
+	for i in pinList:
+		GPIO.output(i, GPIO.HIGH)
+		time.sleep(0.5)
+
+	# Set signal down to all pins one to one in reversed order
+	for i in reversed(pinList):
+		GPIO.output(i, GPIO.LOW)
+		time.sleep(0.5)
+	GPIO.cleanup()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except:
+		GPIO.cleanup()
+
