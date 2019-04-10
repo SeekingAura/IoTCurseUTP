1eea2f74b4017b38634a451a85501fcba9b73b47
Carlos Arturo <carlox216@utp.edu.co>
2019-04-10 14:50:33 -0500

    Update get feed data.py

--- Example code/Adafruit Dashboard/get feed data.py
+++ Example code/Adafruit Dashboard/get feed data.py
@@ -7,19 +7,19 @@ import sys
 from Adafruit_IO import Client
 
 if __name__ == "__main__":
-    if(len(sys.argv)!=5):
+	if(len(sys.argv)!=3):
 		sys.stderr.write('Usage: "{0}" $AdafruitIOUsername $AdafruitIOKey\n'.format(sys.argv[0]))
 		os._exit(1)
-    # Connect to Adafruit IO Server
-    aio=Client(username=sys.argv[1], key=sys.argv[2])
+	# Connect to Adafruit IO Server
+	aio=Client(username=sys.argv[1], key=sys.argv[2])
 
-    # Create feed objects
-    incubatorLigthState=aio.feeds("incubadura0.estado-de-luz")
-    incubatorLigthIntensity=aio.feeds("incubadura0.intensidad-luminica")
-    while True:
-        # get last data from the feeds, Warning should be have any data, not empty
-        incubatorLigthStateData=aio.receive(incubatorLigthState.key)
-        incubatorLigthIntensityData=aio.receive(incubatorLigthIntensity.key)
+	# Link to feeds
+	incubatorLigthState=aio.feeds("incubadura0.estado-de-luz")
+	incubatorLigthIntensity=aio.feeds("incubadura0.intensidad-luminica")
+	while True:
+		# get last data from the feeds, Warning should be have any data, not empty
+		incubatorLigthStateData=aio.receive(incubatorLigthState.key)
+		incubatorLigthIntensityData=aio.receive(incubatorLigthIntensity.key)
 
-        print("Estado del LED {}".format(incubatorLigthStateData.value))
-        print("Intensidad del LED".format(incubatorLigthIntensityData.value))
+		print("Estado del LED {}".format(incubatorLigthStateData.value))
+		print("Intensidad del LED".format(incubatorLigthIntensityData.value))
