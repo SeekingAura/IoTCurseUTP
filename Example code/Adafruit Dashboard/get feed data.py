ee1d7b3c1714f41b9bb09282b4da9352a512004c
Carlos Arturo <carlox216@utp.edu.co>
2019-04-10 12:49:25 -0500

    update get feed data form adafruit dashboard

--- Example code/Adafruit Dashboard/get feed data.py
+++ Example code/Adafruit Dashboard/get feed data.py
@@ -5,8 +5,11 @@ import time
 from Adafruit_IO import Client
 
 if __name__ == "__main__":
+    if(len(sys.argv)!=5):
+		sys.stderr.write('Usage: "{0}" $AdafruitIOUsername $AdafruitIOKey\n'.format(sys.argv[0]))
+		os._exit(1)
     # Connect to Adafruit IO Server
-    aio=Client(username="carlosmore", key="c0354363dc384523bc1022b5fb66d6b7")
+    aio=Client(username=sys.argv[1], key=sys.argv[2])
 
     # Create feed objects
     incubatorLigthState=aio.feeds("incubadura0.estado-de-luz")
